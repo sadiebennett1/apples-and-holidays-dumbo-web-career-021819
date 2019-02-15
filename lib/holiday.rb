35038e6ceb4c7b73db3df5ba4f328d2900c8b4c5
blithe-hacker-0535 <sadiebenn1@gmail.com>
2019-02-15 22:45:22 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -76,13 +76,13 @@ def all_supplies_in_holidays(holiday_hash)
         hol_arr.each do |word|
           hol_str += word.capitalize!
         end
-        suppl_str = supplies.join(",")
+        suppl_str = supplies.join(", ")
 
-        puts "#{hol_str}: #{suppl_str}"
+        puts "   #{hol_str}: #{suppl_str}"
       else
         hol_str.capitalize!
         suppl_str = supplies.join(", ")
-        puts "#  {hol_str}: #{suppl_str}"
+        puts "   #{}{hol_str}: #{suppl_str}"
       end
     end
   end
